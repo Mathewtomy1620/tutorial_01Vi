c90555558a12c2d72fdd7cd5285596704e4693d1
admin <mathewtomy1606@gmail.com>
2022-07-12 12:29:46 +0530

    initial commit

---
+++ bin/tutorial_01.dart
@@ -0,0 +1,7 @@
+//import 'package:tutorial_01/tutorial_01.dart' as tutorial_01;
+void main(){
+  String name = '"Mathew"';
+  double cgpa = 7.2;
+  double percent = cgpa * 10 - 3.75;
+  print('My name is $name, and my graduate percentage is $percent');
+}
